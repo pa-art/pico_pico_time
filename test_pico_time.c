b8490a0b8c33624f349501b3ae6b9f2caa2ccc08
pa-art <pa-art@pa2.so-net.ne.jp>
2021-02-16 19:23:47 +0900

    LCD display function is modified.

--- test_pico_time.c
+++ test_pico_time.c
@@ -12,19 +12,24 @@
 #include "pico/binary_info.h"
 #include "hardware/adc.h"
 
-#define CONV_FACTOR (3.25f / (1 << 12))
+#define CONV_FACTOR (3.25f / (1 << 12)) // ADC data -> voltage
+#define LED_PIN 25  // LED pin#
+#define I2C_SDA 4   // SDA pin#
+#define I2C_SCL 5   // SCL pin#
+#define INTERVAL    500 // [ms]
 
 int count = 0;
 uint16_t temp_dat;
-const uint LED_PIN = 25;
 bool state = true;
 unsigned char chr[1];
 
+// timer handler
 bool disp_counter( struct repeating_timer *t ) {
-
+    // read ADC(temperature) data
     temp_dat = adc_read();
-    gpio_put(LED_PIN, state);
+    // flip LED state
     state = !state;
+    // count up
     count += (state == true);
     chr[0] += (state == true);
 
@@ -33,52 +38,62 @@ bool disp_counter( struct repeating_timer *t ) {
 
 int main() {
 
-    int x, y, c;
-    int cnt = 0;
     char mes_lcd[MAX_CHARS+1];
-    float temp, voltage;
+    float temp, voltage, temp_p, voltage_p;
     struct repeating_timer timer; 
 
     // This example will use I2C0 on GPIO4 (SDA) and GPIO5 (SCL)
     i2c_init(I2C_PORT, 100 * 1000);
-    gpio_set_function(4, GPIO_FUNC_I2C);
-    gpio_set_function(5, GPIO_FUNC_I2C);
-    gpio_pull_up(4);
-    gpio_pull_up(5);
+    gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
+    gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
+    gpio_pull_up(I2C_SDA);
+    gpio_pull_up(I2C_SCL);
     // Make the I2C pins available to picotool
-    bi_decl( bi_2pins_with_func(4, 5, GPIO_FUNC_I2C));
+    bi_decl( bi_2pins_with_func(I2C_SDA, I2C_SCL, GPIO_FUNC_I2C));
     // init LCD
     lcd_init();
+    // initialize LED pin
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
     // init stdio
     stdio_init_all();
-    x = y = 0;
     // init ADC
     adc_init();
     // enable temperature sensor
     adc_set_temp_sensor_enabled(true);
     // select ADC input
     adc_select_input(4);    // ADC selected
-
-    gpio_init(LED_PIN);
-    gpio_set_dir(LED_PIN, GPIO_OUT);
-
-    // set 
-    add_repeating_timer_ms( 500, disp_counter, NULL, &timer );
+    // set timer
+    add_repeating_timer_ms( INTERVAL, disp_counter, NULL, &timer );
+    temp = voltage = temp_p = voltage_p = 0;
 
     while (1) {
 
         voltage = temp_dat * CONV_FACTOR;
         temp = 27 - (voltage - 0.706) / 0.001721;
-        //printf("voltage:%2.2f, tempraure:%2.2f\n", voltage, temp);
+        // make massage from voltage and temp
         sprintf(mes_lcd, "v=%2.3f, T=%2.1f", voltage, temp);
+        // if temp or voltage varies
+        if ((temp_p != temp) || (voltage_p != voltage)) {
+            // clear row 0
+            lcd_set_cursor(0, 0);
+            lcd_string("                ");
+        }
+        // copy temp and voltage
+        temp_p = temp; voltage_p = voltage;
+        // display message
         lcd_set_cursor(0, 0);
         lcd_string(mes_lcd);
-        //sleep_ms(1000);
+        // make message from count
         sprintf(mes_lcd, "count:%d", count);
+        // display message
         lcd_set_cursor(1, 0);
         lcd_string(mes_lcd);
+        // display countup character 
         lcd_set_cursor(1, 15);
         lcd_string(chr);
+        // output state to LED
+        gpio_put(LED_PIN, state);
     }
 
     return 0;
